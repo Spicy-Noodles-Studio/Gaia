04758e3025121a73dd7ece70e34eea7c44fae6da
xRyusey <xRyusey@users.noreply.github.com>
2020-05-12 18:59:59 +0200

    El foco del mando no se mueve a un botón invisible

--- MotorGaia/Src/Gaia/include/InterfaceSystem.h
+++ MotorGaia/Src/Gaia/include/InterfaceSystem.h
@@ -7,19 +7,21 @@
 #include "KeyboardEventListener.h"
 #include "WindowEventListener.h"
 #include "ControllerEventListener.h"
-#include <CEGUI\CEGUI.h>
 #include "UIElement.h"
-#include <string>
 #include "Window.h"
 
+#include <CEGUI\CEGUI.h>
+#include <string>
 #include <map>
 
-namespace CEGUI {
+namespace CEGUI
+{
 	class Window;
 	class OgreRenderer;
 	class String;
 	class EventArgs;
 }
+
 class Window;
 
 typedef std::pair<std::string, std::function<bool()>> UIEvent;
@@ -39,7 +41,6 @@ private:
 
 	std::string ceguiResourceFile;
 
-	
 	void layoutButtonSearch(UIElement* parent);
 	void searchNextVisibleButton(std::string direction);
 	/*------------------------------*/
@@ -47,7 +48,7 @@ private:
 	double deltaX, deltaY;
 #ifdef _DEBUG
 	UIElement* fpsText;
-#endif // _DEBUG
+#endif
 
 	static std::map<std::string, UIEvent> events;
 
@@ -89,9 +90,8 @@ public:
 	UIElement* loadLayout(const std::string& filename);
 	void initDefaultResources(const std::string& filename);
 
-	void initControllerMenuInput(UIElement* newRoot=nullptr);
+	void initControllerMenuInput(UIElement* newRoot = nullptr);
 	void clearControllerMenuInput();
-
 
 	/// Percentage of scrollbar that changes when moved by controller or keyboard
 	void setScrollbarControllerAmount(float percent);
